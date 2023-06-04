3e04ba74a6e8aeccc9bb358e105ed12fc7efbddc
mert-donmez <mertysta2@gmail.com>
2023-06-04 19:36:08 +0300

    bottom sheet modal  updated

--- rentaxi/components/HomeScreen.js
+++ rentaxi/components/HomeScreen.js
@@ -1,136 +1,167 @@
-import React, { useCallback, useMemo, useRef,useState,useEffect } from "react";
-import { View, Text, StyleSheet,Image, SafeAreaView } from "react-native";
+import React, {
+  useCallback,
+  useMemo,
+  useRef,
+  useState,
+  useEffect,
+} from "react";
+import { View, Text, StyleSheet, Image, SafeAreaView } from "react-native";
 import BottomSheet from "@gorhom/bottom-sheet";
 import MapView from "react-native-maps";
-import {MaterialIcons} from '@expo/vector-icons';
+import { MaterialIcons } from "@expo/vector-icons";
 
 const HomeScreen = () => {
   const snapPoints = useMemo(() => ["20%", "50%"], []);
-  
 
   return (
     <View style={styles.container}>
-      <MapView 
-      style={styles.map} 
-      mapType="terrain" 
-      userInterfaceStyle="light" 
-      showsUserLocation={true} 
-      userLocationPriority={'high'}
-      showsCompass={false}
-      showsTraffic={true}
-       
-       >
+      <MapView
+        style={styles.map}
+        mapType="terrain"
+        userInterfaceStyle="light"
+        showsUserLocation={true}
+        userLocationPriority={"high"}
+        showsCompass={false}
+        showsTraffic={true}
+      >
         <View style={styles.titleWrapper}>
-            <View style={{backgroundColor:'white',height:50,width:50,justifyContent:'center',alignItems:'center',borderRadius:50,shadowColor: "#000",
-shadowOffset: {
-	width: 0,
-	height: 2,
-},
-shadowOpacity: 0.25,
-shadowRadius: 3.84,}}>
-                <MaterialIcons name="menu" size={40}/>
-            </View>
-            <Image source={require('../assets/images/profile.png')} style={styles.profilePicture}/>
+          <View
+            style={{
+              backgroundColor: "white",
+              height: 50,
+              width: 50,
+              justifyContent: "center",
+              alignItems: "center",
+              borderRadius: 50,
+              shadowColor: "#000",
+              shadowOffset: {
+                width: 0,
+                height: 2,
+              },
+              shadowOpacity: 0.25,
+              shadowRadius: 3.84,
+            }}
+          >
+            <MaterialIcons name="menu" size={40} />
+          </View>
+          <Image
+            source={require("../assets/images/profile.png")}
+            style={styles.profilePicture}
+          />
         </View>
-
-        
-            <View style={styles.addressWrapper}>
-                <MaterialIcons name="search" size={30} style={{marginLeft:10}} />
-                <View style={styles.whereYouGoingWrapper}>
-                <Text style={styles.addressText}>Where are you going ?</Text>
-                <View style={styles.divider}/>
-                </View>
-                
-            </View>
-           
-        
-
-      
-      </MapView> 
-      {/* <BottomSheet 
+      </MapView>
+      <BottomSheet
         index={0}
         snapPoints={snapPoints}
         style={styles.bottomModalstyle}
       >
-        <View style={styles.contentContainer}>
-          <Text>Test</Text>
+        <View style={styles.addressWrapper}>
+          <View style={styles.whereYouGoingWrapper}>
+            <MaterialIcons
+              name="my-location"
+              size={30}
+              style={{ marginLeft: 10 }}
+              color={"#54B435"}
+            />
+            <View style={styles.addressTitleWrapper}>
+              <Text style={styles.upperTitle}>PICKUP</Text>
+              <Text style={styles.addressText}>Current Location</Text>
+            </View>
+          </View>
+          <View style={styles.middleDividerWrapper}>
+            <MaterialIcons
+              name="more-vert"
+              size={30}
+              style={{ marginLeft: 10 }}
+            />
+            <View style={styles.divider} />
+          </View>
+          <View style={styles.whereYouGoingWrapper}>
+            <MaterialIcons
+              name="location-on"
+              size={30}
+              style={{ marginLeft: 10 }}
+              color={"#C21010"}
+            />
+            <View style={styles.addressTitleWrapper}>
+              <Text style={styles.upperTitle}>DROP-OFF</Text>
+              <Text style={styles.addressText}>University</Text>
+            </View>
+          </View>
         </View>
-      </BottomSheet>  */}
+      </BottomSheet>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    titleWrapper:{
-        flexDirection:'row',
-        alignItems:'center',
-        justifyContent:'space-between',
-        marginHorizontal:20,
-        marginTop:70,
-    },
-    profilePicture:{
-        width:40,
-        height:40,
-        borderRadius:40,
-    },
-    whereYouGoingWrapper:{
-        justifyContent:'center',
-    },
-    divider:{
-        height:StyleSheet.hairlineWidth,
-        backgroundColor:'black',
-        marginTop:5,
-        marginLeft:10,
-        width:'100%'
-    },
-    addressText:{
-        marginLeft:10,
-        color:'grey',
-        fontSize:20,
-    },
-    addressTextWrapper:{
-        backgroundColor:'#DDE6ED',
-        marginHorizontal:20,
-        height:30,
-        justifyContent:'center',
-        borderRadius:5,
+  upperTitle: {
+    color: "grey",
+    fontSize: 10,
+  },
+  addressTitleWrapper: {
+    marginLeft: 20,
+  },
+  middleDividerWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: 20,
+    marginVertical: 5,
+  },
+  titleWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginHorizontal: 20,
+    marginTop: 70,
+  },
+  profilePicture: {
+    width: 40,
+    height: 40,
+    borderRadius: 40,
+  },
+  whereYouGoingWrapper: {
+    alignItems: "center",
+    flexDirection: "row",
+    marginLeft: 20,
+  },
+  divider: {
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: "black",
+    marginLeft: 10,
+    width: "80%",
+  },
+  addressText: {
+    color: "black",
+    fontSize: 16,
+  },
+  addressTextWrapper: {
+    backgroundColor: "#DDE6ED",
+    marginHorizontal: 20,
+    height: 30,
+    justifyContent: "center",
+    borderRadius: 5,
+  },
 
-    },
-    
-    profilePicture:{
-        height:60,
-        width:60,
-        borderRadius:60,
-    },
-    addressWrapper:{
-        position:'absolute',
-        marginHorizontal:20,
-        backgroundColor:'white',
-        height:50,
-        alignItems:'center',
-        borderRadius:10,
-        bottom:'10%',
-        width:'90%',
-        flexDirection:'row',
-        shadowColor: "#000",
-shadowOffset: {
-	width: 0,
-	height: 2,
-},
-shadowOpacity: 0.25,
-shadowRadius: 3.84,
-        
-       
-
-
-    },
+  profilePicture: {
+    height: 60,
+    width: 60,
+    borderRadius: 60,
+  },
+  addressWrapper: {},
   bottomModalstyle: {
-    
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 10,
   },
-  container:{
-    flex:1
+  container: {
+    flex: 1,
   },
- 
+
   contentContainer: {
     flex: 1,
     alignItems: "center",
