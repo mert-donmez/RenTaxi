eac13f4e39c18abf78ac0f4f6dad895ccd52983a
mert-donmez <mertysta2@gmail.com>
2023-06-11 17:36:28 +0300

    start component updates

--- rentaxi/components/StartAppScreen.js
+++ rentaxi/components/StartAppScreen.js
@@ -7,13 +7,13 @@ const StartAppScreen = ({ navigation }) => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar style='light' />
+      <StatusBar style='dark' />
       <View style={styles.titleWrapper}>
         <View style={styles.mainTitleWrapper}>
-          <MaterialIcons name='near-me' size={30} color={'white'}/>
+          <MaterialIcons name='near-me' size={30} color={'#F5CE55'}/>
             <Text style={styles.mainTitle}>RenTaxi</Text>
         </View>
-        <MaterialIcons name='info' size={20} color={'white'}/>
+        <MaterialIcons name='info' size={20} color={'black'}/>
       </View>
       <View style={styles.divider} />
       <View style={styles.subTitleWrapper}>
@@ -21,11 +21,11 @@ const StartAppScreen = ({ navigation }) => {
         <Text style={styles.secondarySubTitleText}>Comfortable rides around the city</Text>
       </View>
       <View style={styles.carImageWrapper}>
-      <Image source={require('../assets/images/white-car-top.png')} style={styles.carImage} resizeMode='contain'/>
+      <Image source={require('../assets/images/grey-car-top.png')} style={styles.carImage} resizeMode='contain'/>
       </View>
       <TouchableOpacity style={styles.startButton} onPress={() => navigation.navigate('Register')}>
         <Text style={styles.buttonText}>Start</Text>
-        <MaterialIcons name='arrow-forward-ios' color={'black'} size={30}/>
+        <MaterialIcons name='arrow-forward-ios' color={'white'} size={30}/>
       </TouchableOpacity>
     </SafeAreaView>
   )
@@ -39,7 +39,7 @@ const styles = StyleSheet.create({
     bottom:'10%',
     justifyContent:'center',
     alignItems:'center',
-    backgroundColor:'#F5CE55',
+    backgroundColor:'black',
     borderRadius:20,
     height:'8%',
     width:'80%',
@@ -48,12 +48,11 @@ const styles = StyleSheet.create({
     flexDirection:'row'
   },
   buttonText:{
-    color:'black',
+    color:'white',
     fontSize:30,
     fontWeight:'500',
   },
   carImageWrapper:{
-    marginRight:'7%',
     alignItems:'center',
     marginTop:'15%',
   },
@@ -66,7 +65,7 @@ const styles = StyleSheet.create({
     marginTop:'20%',
   },
   mainSubTitleText:{
-    color:'white',
+    color:'black',
     fontSize:30,
 
   },
@@ -84,11 +83,11 @@ secondarySubTitleText:{
   },
   container:{
     flex:1,
-    backgroundColor:'black',
+    backgroundColor:'white',
   
   },
   mainTitle:{
-    color:'#F5CE55',
+    color:'black',
     fontSize:30,
 
   },
